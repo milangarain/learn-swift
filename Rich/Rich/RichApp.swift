66aad22b6c611cdc0c90ff418dcf1b429d324a0a
Milan <Milangarain1999@gmail.com>
2024-08-18 20:38:50 +0530

    initial commit

---
+++ Rich/Rich/RichApp.swift
@@ -0,0 +1,17 @@
+//
+//  RichApp.swift
+//  Rich
+//
+//  Created by Milan Garain on 18/08/24.
+//
+
+import SwiftUI
+
+@main
+struct RichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
